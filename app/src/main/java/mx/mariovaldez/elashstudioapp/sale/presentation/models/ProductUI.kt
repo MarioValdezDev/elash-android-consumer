9c7a1117e0862169db3a1714a11e75f99393ca1b
MarioValdezDev <60115634+MarioValdezDev@users.noreply.github.com>
2023-06-06 11:09:07 -0700

    Develop into Main (#31)
    * add animations
    
    * add ToolbarKtx.kt for toolbar binding
    
    * add buttons styles
    
    * add layout_toolbar.xml
    
    * update database
    
    * add product view UI
    
    * add product UI model
    
    * add product adapter
    
    * add sale activity that contains fragments
    
    * update navigation
    
    * add choose product UI
    
    ---------
    
    Co-authored-by: Mario Antonio Flores Valdez <mario.flores@coppel.com>

---
+++ app/src/main/java/mx/mariovaldez/elashstudioapp/sale/presentation/models/ProductUI.kt
@@ -0,0 +1,8 @@
+package mx.mariovaldez.elashstudioapp.sale.presentation.models
+
+internal data class ProductUI(
+    val idProduct: String,
+    val name: String,
+    val price: String,
+    val image: String,
+)
