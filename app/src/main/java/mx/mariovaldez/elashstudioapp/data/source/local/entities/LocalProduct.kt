e3e2f532d60f20741031bf23476fdfcef60f7077
Mario Antonio Flores Valdez <mario.flores@coppel.com>
2023-06-27 09:22:38 -0700

    update localProduct model

--- app/src/main/java/mx/mariovaldez/elashstudioapp/data/source/local/entities/LocalProduct.kt
+++ app/src/main/java/mx/mariovaldez/elashstudioapp/data/source/local/entities/LocalProduct.kt
@@ -2,6 +2,7 @@ package mx.mariovaldez.elashstudioapp.data.source.local.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import mx.mariovaldez.elashstudioapp.sale.domain.models.Product
 
 @Entity(
     tableName = "product"
@@ -9,5 +10,22 @@ import androidx.room.PrimaryKey
 data class LocalProduct(
     @PrimaryKey val idProduct: String,
     val name: String,
-    val price: String
+    val price: Double,
+    val image: String,
 )
+
+fun LocalProduct.toExternal() = Product(
+    idProduct = idProduct,
+    name = name,
+    price = price,
+    image = image
+)
+
+fun Product.toLocal() = LocalProduct(
+    idProduct = idProduct,
+    name = name,
+    price = price,
+    image = image
+)
+
+fun List<LocalProduct>.toExternal() = map(LocalProduct::toExternal)
